44c64e76280bf8b2bfc6d8e2c422669b033dd88f
Christopher Schmidt <christopher.schmidt@here.com>
2013-11-18 00:31:37 -0500

    Add new load balancer.
    After thinking about this over the past day, it has bothered me
    repeatedly that I built such a hacky solution. Change it up to
    be a better solution; now it scales with the number of things
    being balanced over, instead of the sum of the weights.

--- loadbalancer/solution.py
+++ loadbalancer/solution.py
@@ -12,3 +12,25 @@ class Balancer:
     def request(self):
         balancer = random.randint(0,self.total-1)
         return self.match_list[balancer]
+
+# After writing this, I realized that my solution above is hacky as
+# heck; it works fine for the case where the sum is a small number and
+# the weights are all integers, but fails in any other case. This bugged
+# me enough to do something about it.
+class Balancer2:
+    def __init__(self, weights):
+        self.weights = weights
+        self.total = sum([weight[1] for weight in weights])
+        start = 0
+        match_list = []
+        cur = 0
+        for weight in self.weights:
+            match_list.append((cur, cur + weight[1], weight[0]))
+            cur += weight[1]
+        self.match_list = match_list       
+    def request(self):
+        balancer = random.random()*self.total
+        for item in self.match_list:
+            if balancer >= item[0] and balancer < item[1]:
+                return item[2]
+        print "What?!"        
--- loadbalancer/test.py
+++ loadbalancer/test.py
@@ -1,7 +1,20 @@
 import solution
+import time
 
 b = solution.Balancer([['a', 1], ['b', 1], ['c', 8]])
 matches = {'a': 0, 'b': 0, 'c':0}
 for i in range(20000):
     matches[b.request()] += 1
 print matches    
+t = time.time()
+b = solution.Balancer([['a', 10000000], ['b', 1]])
+matches = {'a': 0, 'b': 0}
+for i in range(20000):
+    matches[b.request()] += 1
+print matches , time.time() - t   
+t = time.time()
+b = solution.Balancer2([['a', 10000000], ['b', 1]])
+matches = {'a': 0, 'b': 0}
+for i in range(20000):
+    matches[b.request()] += 1
+print matches , time.time() - t   
